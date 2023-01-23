cf32b67a5944c83854d240c23f146b53fdca0739
Jonathan Pohahau <j.pohahau@outlook.com>
2023-01-23 15:12:41 -0800

    adding the content with the syntax

--- Develop/index.js
+++ Develop/index.js
@@ -1,73 +1,77 @@
 // TODO: Include packages needed for this application
+// making a const inquirer to it constantly has the value of require method requiringinquirer
 const inquirer = require("inquirer");
+// making a const fs to it constantly has the value of require method requiring fs
 const fs = require("fs");
+const generateMarkdown = require ("./utils/generateMarkdown");
+const fileName = "README.md";
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input",
         message: "What is your Project title?",
-        input: "title",
-    },
-    {
-        type: "input",
-        message: "What is your table of contents?",
-        input: "table of contents",
+        name: "title",
     },
     {
         type: "input",
         message: "What is the description of your Project?",
-        input: "description",
+        name: "description",  
     },
     {
         type: "input",
         message: "How do you install your Project?",
-        input: "installation",
+        name: "installation",
     },
     {
         type: "input",
         message: "How do you use your Project?",
-        input: "usage",
+        name: "usage",
     },
     {
         type: "input",
         message: "What are your Contribution guide lines?",
-        input: "contribution",
+        name: "contribution",
     },
     {
         type: "input",
         message: "What are your test instructions?",
-        input: "tests",
+        name: "tests",
     },
     {
-        type: "input",
+        type: "checkbox",
         message: "Select the license that suits your needs",
         name: "license",
-        choices: ["MIT", "Apache", "GNU", "WordPress", "Rust"]
+        choices: ["mit", "gpl-3.0", "isc", "cc0-1.0", "CC-BY-4.0", "CC-BY-SA-4.0", "cern-ohl-p-2.0", "cern-ohl-w-2.0"]
     },
     {
         type: "input",
         message: "What is your Github user name?",
-        input: "user name",
+        name: "userName",
     },
     {
         type: "input",
         message: "What is your email?",
-        input: "email",
-    },
-    {
-        type: "input",
-        message: "What is your Github link?",
-        input: "link",
+        name: "email",
     },
 ];
-console.log(questions)
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
+    const markdown = generateMarkdown(data)
+    fs.writeFile(fileName, markdown, function(err){
+        if(err) throw err;
+        console.log("yessah!😁")
+    })
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(function(data) {
+        writeToFile(fileName, data)
+    }); 
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,21 +1,80 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  if(!license){
+    return ''
+  } else {
+  return `![Badge](https://img.shields.io/badge/License-${license}-blueviolet)`
+  }
 
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(!license){
+    return ''
+  } else {
+    return `https://choosealicense.com/licenses/${license}/`
+  }
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(!license){
+    return ''
+  } else {
+    return `This application is covered under the ${license} license.`
+  }
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title} ##${data.descrption}
+  return `# ${data.title}
 
+
+  ## Description
+
+  ${data.description}
+  
+
+  ## Table of Contents (Optional)
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+
+
+  ## Installation
+  ${data.installation}
+
+
+  ## Usage
+  ${data.usage}
+
+
+  ## License
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(data.license)}
+  ${renderLicenseBadge(data.license)}
+
+
+  ## Contribution
+  ${data.usage}
+
+
+  ## Tests
+  ${data.tests}
+
+
+  ## Questions
+  **Github Link** https://github.com/${data.userName}
+
+  **Email** ${data.email}
 `;
 
 }
