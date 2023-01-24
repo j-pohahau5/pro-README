3d187a38ec638202218892a9d73f45f72e436ed5
j-pohahau5 <119136195+j-pohahau5@users.noreply.github.com>
2023-01-23 18:53:03 -0800

    added comments to index.js

--- Develop/index.js
+++ Develop/index.js
@@ -3,10 +3,15 @@
 const inquirer = require("inquirer");
 // making a const fs to it constantly has the value of require method requiring fs
 const fs = require("fs");
-const generateMarkdown = require ("./utils/generateMarkdown");
+// used this generateMarkdown to require the generateMarkdown.js
+const generateMarkdown = require("./utils/generateMarkdown");
+// created  fileName so it can be the file that is created
 const fileName = "README.md";
 
 // TODO: Create an array of questions for user input
+// used the types as input used 
+// used name to refer to them when calling the data
+// used message to ask user the question
 const questions = [
     {
         type: "input",
@@ -58,8 +63,12 @@ const questions = [
 
 
 // TODO: Create a function to write README file
+// used function  to write to the file name and include the data
 function writeToFile(fileName, data) {
+    // used markdown to grab generateMarkdown and its data
     const markdown = generateMarkdown(data)
+    // all this function  is saying to create the  file with the generate mark down data
+    //  also saying if there is a error to return an error if now console.log yessah!😁
     fs.writeFile(fileName, markdown, function(err){
         if(err) throw err;
         console.log("yessah!😁")
@@ -67,6 +76,7 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
+// this function init is saying to give the user the questions using the prompt method  functioning  writeToFile
 function init() {
     inquirer.prompt(questions).then(function(data) {
         writeToFile(fileName, data)
